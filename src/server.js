f77c10567b574c7601c297c837aff18caa1a8d60
pemulmruthunjaya <pemul.mruthunjaya@gmail.com>
2026-02-16 06:26:48 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,9 +6,16 @@ const fs = require("fs");
 const express = require("express");
 const mysql = require("mysql2/promise");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
